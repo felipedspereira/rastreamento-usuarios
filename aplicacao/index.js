70d030097ea56756f5805184467f652bc3fe0eb9
Felipe Pereira <felipe.dspereira@gmail.com>
2017-12-17 14:40:13 -0400

    Preparação de template para aplicação de monitoração

--- aplicacao/index.js
+++ aplicacao/index.js
@@ -1,43 +1,23 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var path = require('path');
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// CORS
+app.set('views', path.join(__dirname, '/app/views'));
+app.set('view engine', 'ejs');
+
+// Cors
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
     next();
 });
 
-/**
- * Apis
- */
-
-var user = {email : '', activities : []};
-
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-app.post('/enableActivity', function (req, res) {
-    user.email = req.body.email;
-    res.send();
-});
-
-app.post('/activity', function (req, res) {
-    var userActivities = req.body;
-
-    userActivities.forEach(item => {
-        user.activities.push(item);
-    });
-
-    console.log(JSON.stringify(user, null, 4))
-
-    return res.send('message successfull');
-});
+// Import routes
+var activityRoutes = require('./app/routes/activity')(app);
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
