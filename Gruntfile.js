69f58b23cb9a45e635ed474f499e1ea354962cfa
Felipe Pereira <felipe@prodam.am.gov.br>
2017-12-18 11:29:10 -0400

    including settings in gruntfile to deploy for enviroment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,54 @@
 module.exports = function (grunt) {
+    grunt.initConfig({
+        clean: {
+            all: ['dist/']
+        },
+        copy: {
+            index: {
+                src: 'index.html',
+                dest: 'dist/index.html',
+            },
+            pages: {
+                src: 'pages/**/*.html',
+                dest: 'dist/',
+            },
+            js: {
+                src: 'js/trackingJS.min.js',
+                dest: 'dist/js/trackingJS.min.js',
+            },
+            images: {
+                src: 'images/**/*.jpg',
+                dest: 'dist/',
+            },
+            css: {
+                src: 'css/**/*.css',
+                dest: 'dist/',
+            }
+        },
+        'string-replace': {
+            dev: {
+                files: {
+                    'dist/': ['pages/**/*.html', 'index.html'],
+                },
+                options: {
+                    replacements: [{
+                            pattern: 'https://rastreamento-usuarios-app.herokuapp.com/activity',
+                            replacement: 'http://localhost:3000/activity'
+                        },
+                        {
+                            pattern: 'https://rastreamento-usuarios-app.herokuapp.com/enableActivity',
+                            replacement: 'http://localhost:3000/enableActivity'
+                        }
+                    ]
+                }
+            }
+        }
+    });
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
-};
+    grunt.loadNpmTasks('grunt-string-replace');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('build-dev', ['clean', 'copy', 'string-replace:dev']);
+    grunt.registerTask('build-prod', ['clean', 'copy']);
+}
